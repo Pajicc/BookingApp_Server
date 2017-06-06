746cc11cc0278233b00ef3db0ef09a3b2243f259
Pajicc <srki94@live.com>
2017-06-06 13:45:54 +0200

    Inserted data in DB and started implementing Controllers

---
+++ BookingApp/BookingApp/Controllers/AccomodationsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BookingApp.Models;
+
+namespace BookingApp.Controllers
+{
+
+    [RoutePrefix("api")]
+    public class AccomodationsController : ApiController
+    {
+        private BAContext db = new BAContext();
+
+        [HttpGet]
+        [Route("Accomodations", Name = "Acc")]
+        public IQueryable<Accomodation> GetAccomodations()
+        {
+            return db.Accomodations;
+        }
+
+        [HttpGet]
+        [Route("Accomodations/{id}")]
+        [ResponseType(typeof(Accomodation))]
+        public IHttpActionResult GetAccomodation(int id)
+        {
+            Accomodation accomodation = db.Accomodations.Find(id);
+            if (accomodation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(accomodation);
+        }
+
+        [HttpPut]
+        [Route("Accomodations/{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAccomodation(int id, Accomodation accomodation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != accomodation.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(accomodation).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AccomodationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpPost]
+        [Route("Accomodations")]
+        [ResponseType(typeof(Accomodation))]
+        public IHttpActionResult PostAccomodation(Accomodation accomodation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Accomodations.Add(accomodation);
+            db.SaveChanges();
+
+            return CreatedAtRoute("Acc", new { id = accomodation.Id }, accomodation);
+        }
+
+        [HttpPost]
+        [Route("Accomodations/{id}")]
+        [ResponseType(typeof(Accomodation))]
+        public IHttpActionResult DeleteAccomodation(int id)
+        {
+            Accomodation accomodation = db.Accomodations.Find(id);
+            if (accomodation == null)
+            {
+                return NotFound();
+            }
+
+            db.Accomodations.Remove(accomodation);
+            db.SaveChanges();
+
+            return Ok(accomodation);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AccomodationExists(int id)
+        {
+            return db.Accomodations.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- BookingApp/BookingApp/Migrations/Configuration.cs
+++ BookingApp/BookingApp/Migrations/Configuration.cs
@@ -31,6 +31,77 @@ namespace BookingApp.Migrations
             //    );
             //
 
+            context.AccomodationTypes.AddOrUpdate(
+              a => a.Name,
+              new AccomodationType { Name = "Hotel"},
+              new AccomodationType { Name = "House" }
+            );
+
+            context.Countries.AddOrUpdate(
+            c => c.Name,
+            new Country { Name = "Germany" }
+            );
+
+            context.AppUsers.AddOrUpdate(
+            au => au.Username,
+            new AppUser { Username = "user1", Password = "user1", Email = "user1@live.com", FullName = 1 },
+            new AppUser { Username = "user2", Password = "user2", Email = "user2@live.com",FullName = 4 },
+            new AppUser { Username = "user3", Password = "user3", Email = "user3@live.com",FullName = 3 },
+            new AppUser { Username = "user4", Password = "user4", Email = "user4@live.com",FullName = 2},
+            new AppUser { Username = "owner1", Password = "owner1", Email = "owner1@live.com", FullName = 2 },
+             new AppUser { Username = "owner2", Password = "owner2", Email = "owner2@live.com", FullName = 2 },
+             new AppUser { Username = "admin", Password = "admin", Email = "admin@live.com", FullName = 2 }
+            );
+
+            context.SaveChanges();
+
+            context.Regions.AddOrUpdate(
+            r => r.Name,
+            new Region { Name = "East Germany", CountryId = 1 },
+            new Region { Name = "West Germany", CountryId = 1}
+            );
+
+            context.SaveChanges();
+
+            context.Places.AddOrUpdate(
+            p => p.Name,
+            new Place { Name = "Berlin", RegionId = 1 },
+            new Place { Name = "Frankfurt", RegionId = 2 }
+            );
+
+            context.SaveChanges();
+
+            context.Accomodations.AddOrUpdate(
+            a => a.Name,
+            new Accomodation { Name = "Hotel Park", AverageGrade = 5, Address = "Main Street", PlaceId = 1, Longtitude = 24.600, Latitude = 25.880, Description = "Hotel", ImageURL = "c:/users/img1.jpg", AppUserId = 5, AccomodationTypeId = 1, Approved = true },
+           new Accomodation { Name = "Green House", AverageGrade = 4, Address = "5th Street", PlaceId = 2, Longtitude = 34.600, Latitude = 45.880, Description = "House", ImageURL = "c:/users/img2.jpg", AppUserId = 6, AccomodationTypeId = 2, Approved = true }
+            );
+
+           context.SaveChanges();
+
+           context.Comments.AddOrUpdate(
+           c => c.Text,
+           new Comment { Text = "Great place", Grade = 4, AppUsersId = 1, AccomodationId = 1 },
+           new Comment { Text = "Ok", Grade = 3, AppUsersId = 2, AccomodationId = 2 },
+           new Comment { Text = "Very bad", Grade = 1, AppUsersId = 3, AccomodationId = 1 },
+           new Comment { Text = "Very nice", Grade = 5, AppUsersId = 4, AccomodationId = 2 }
+           );
+
+            context.Rooms.AddOrUpdate(
+            r => r.RoomNumber,
+            new Room { Description = "Room1", BedCount = 3, PricePerNight = 1500, AccomodationId = 1, RoomNumber = 1 },
+            new Room { Description = "Room2", BedCount = 2, PricePerNight = 1000, AccomodationId = 2, RoomNumber = 2 }
+            );
+
+            context.SaveChanges();
+
+            context.RoomReservations.AddOrUpdate(
+            r => r.StartDate,
+            new RoomReservations { StartDate = DateTime.Now, EndDate = DateTime.Today, AppUserId = 1, RoomId = 1, Timestamp = DateTime.Now}
+            );
+
+            context.SaveChanges();
+
             if (!context.Roles.Any(r => r.Name == "Admin"))
             {
                 var store = new RoleStore<IdentityRole>(context);
@@ -53,7 +124,7 @@ namespace BookingApp.Migrations
             {
                 var store = new RoleStore<IdentityRole>(context);
                 var manager = new RoleManager<IdentityRole>(store);
-                var role = new IdentityRole { Name = "AppUser" };
+                var role = new IdentityRole { Name = "AppUser"};
 
                 manager.Create(role);
             }
@@ -63,7 +134,7 @@ namespace BookingApp.Migrations
 
             if (!context.Users.Any(u => u.UserName == "admin"))
             {
-                var user = new BAIdentityUser() { Id = "admin", UserName = "admin", Email = "admin@yahoo.com", PasswordHash = BAIdentityUser.HashPassword("admin")};
+                var user = new BAIdentityUser() { Id = "admin", UserName = "admin", Email = "admin@yahoo.com", PasswordHash = BAIdentityUser.HashPassword("admin"), appUserId = 7};
                 userManager.Create(user);
                 userManager.AddToRole(user.Id, "Admin");
             }
